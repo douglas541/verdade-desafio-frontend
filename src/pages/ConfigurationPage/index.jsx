6aad61826b5f6a958b808e912d390f0803e3c388
douglas541 <douglas541@discente.ufg.br>
2020-08-28 00:43:12 -0300

    Estruturação básica das pages

---
+++ src/pages/ConfigurationPage/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ConfigurationPage(){
+   return(
+      <div>
+         <h1>ConfigurationPage</h1>
+      </div>
+   )
+}
---
+++ src/pages/PreGamePage/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PreGamePage(){
+   return(
+      <div>
+         <h1>PreGamePage</h1>
+      </div>
+   )
+}
